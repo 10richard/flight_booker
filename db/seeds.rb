69cb3bf74c919a1eac80e9777525ae782bdbb0c8
Richard Ten <rten04@icloud.com>
2023-06-17 15:50:30 -0400

    Migrate db after genning Airport and Flight model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = Airport.create([{ code: 'NYC' }, { code: 'LAX' }, { code: 'MIA' }, { code: 'SFO' }, { code: 'DEN' }])
