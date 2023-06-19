8d0b728ac9c053a5123d3cfd26ac243c2fb9c08e
Richard Ten <rten04@icloud.com>
2023-06-19 16:25:05 -0400

    Store unique available Flight dates in @dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,7 @@ class FlightsController < ApplicationController
 
     def index
         @flights = Flight.all
+        dates = Flight.select(:start)
+        @dates = (dates.map {|d| d.start.strftime("%B %d, %Y")}).uniq
     end
 end
