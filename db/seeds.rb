d9a5f308388f57ac82c922289ea934c5f4124a7f
Richard Ten <rten04@icloud.com>
2023-06-18 22:06:11 -0400

    Seed database with Airports and Flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-airports = Airport.create([{ code: 'NYC' }, { code: 'LAX' }, { code: 'MIA' }, { code: 'SFO' }, { code: 'DEN' }])
+#airports = Airport.create([{ code: 'NYC' }, { code: 'LAX' }, { code: 'MIA' }, { code: 'SFO' }, { code: 'DEN' }])
+#Flight.create(departure_airport: 2, arrival_airport: 4, start: DateTime.now + 3.days, duration: 1)
+#Flight.create(departure_airport: 3, arrival_airport: 1, start: DateTime.now + 6.days, duration: 3)
+#Flight.create(departure_airport: 5, arrival_airport: 2, start: DateTime.now + 7.days, duration: 2)
+#Flight.create(departure_airport: 4, arrival_airport: 1, start: DateTime.now + 10.days, duration: 5)
+#Flight.create(departure_airport: 2, arrival_airport: 3, start: DateTime.now + 14.days, duration: 5)
