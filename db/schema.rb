82775e16b0924c84fad453aa7bc07fe777cde15f
Richard Ten <rten04@icloud.com>
2023-06-20 14:53:21 -0400

    Migrate db after changing departure_time data type in Flights table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_183307) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_185043) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_183307) do
     t.integer "departure_airport"
     t.integer "arrival_airport"
     t.date "departure_date"
-    t.time "departure_time"
+    t.string "departure_time"
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
