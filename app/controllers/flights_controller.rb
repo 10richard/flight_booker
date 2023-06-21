36a11fecdb985674a4996e18684d1d3cb03b43de
Richard Ten <rten04@icloud.com>
2023-06-21 10:59:54 -0400

    Fix/modify Flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,15 @@ class FlightsController < ApplicationController
 
     def index
         @flights = Flight.all
-        dates = Flight.select(:start)
-        @dates = (dates.map {|d| d.start.strftime("%B %d, %Y")}).uniq
+        @dates = Flight.select(:departure_date).distinct
+        @origin_airport = Airport.find_by(id: params[:origin_id])
+        @arrival_airport = Airport.find_by(id: params[:dest_id])
+        @flight_date = params[:flight_date]
+        @available_flights = Flight.user_search(query_params)
+    end
+
+    private
+    def query_params
+        params.permit(:origin_id, :dest_id, :flight_date, :tickets)
     end
 end
