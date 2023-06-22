a4ba1ab139cffbf6c167d5132b324a8645c5ae1e
Richard Ten <rten04@icloud.com>
2023-06-21 23:54:20 -0400

    Add nested_attributes_for :passengers

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-    validates :flight_id, presence: true
-
     belongs_to :flight
-    has_many :passengers
+    has_many :passengers, dependent: :destroy
+
+    accepts_nested_attributes_for :passengers
 end
